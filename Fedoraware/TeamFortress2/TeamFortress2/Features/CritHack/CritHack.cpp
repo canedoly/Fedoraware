f4f2bc61794a742c700ba53ed40aa34a2117f51d
canedoly <81353241+canedoly@users.noreply.github.com>
2022-05-25 15:09:07 +0200

    ok

--- Fedoraware/TeamFortress2/TeamFortress2/Features/CritHack/CritHack.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/CritHack/CritHack.cpp
@@ -32,7 +32,7 @@ bool CCritHack::ShouldCrit()
 }
 
 /* Returns the next crit command number */
-int CCritHack::NextCritTick(const CUserCmd* pCmd, int loops = (Vars::CritHack::Critloops.m_Var))
+int CCritHack::NextCritTick(const CUserCmd* pCmd, int loops = (Vars::CritHack::CritLoops.m_Var))
 {
 	static int previousWeapon = 0;
 	static int previousCrit = 0;
