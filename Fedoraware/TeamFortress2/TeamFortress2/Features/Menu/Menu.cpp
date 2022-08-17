623a498669d30b2945646aeb0c0d919c3a2bf111
canedoly <81353241+canedoly@users.noreply.github.com>
2022-08-17 15:36:08 +0200

    debug

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Menu/Menu.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Menu/Menu.cpp
@@ -1727,6 +1727,7 @@ void CMenu::SettingsWindow()
 		if (ColorPicker("Menu accent", Vars::Menu::Colors::MenuAccent)) { LoadStyle(); } SameLine(); Text("Menu accent");
 		if (Checkbox("Alternative Design", &Vars::Menu::ModernDesign)) { LoadStyle(); }
 		Checkbox("Show DVD bounce", &Vars::Menu::ShowDVD.Value);
+		Checkbox("Debug", &Vars::Debug::DebugInfo.Value);
 		if (Checkbox("Menu Vignette", &Vars::Menu::Vignette.Value)){
 			I::ViewRender->SetScreenOverlayMaterial(nullptr);
 		}
