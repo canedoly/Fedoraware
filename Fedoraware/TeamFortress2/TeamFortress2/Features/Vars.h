22812b689a6cadc382ad18725fe1f4546d4325b7
canedoly <81353241+canedoly@users.noreply.github.com>
2022-08-20 20:09:27 +0200

    fakelag correction and why does it not shoot

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Vars.h
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Vars.h
@@ -123,7 +123,7 @@ namespace Vars
 			inline CVar<bool> WaitForCharge{ false };
 			inline CVar<bool> SpectatedSmooth{ false };
 			inline CVar<bool> ScopedOnly{ false };
-			inline CVar<bool> AimAtLag{ false };
+			inline CVar<bool> FakelagCorrection{ false };
 			inline CVar<int> UnlagTicks{ 0 };
 			inline CVar<bool> AutoScope{ false };
 			inline CVar<bool> AutoRev{ false };
