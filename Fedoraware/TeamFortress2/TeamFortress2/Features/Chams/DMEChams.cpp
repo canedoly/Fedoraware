9cbd6cef237424fb6e4e5557123363f9580550d9
canedoly <81353241+canedoly@users.noreply.github.com>
2022-08-26 17:37:17 +0200

    ahhh

--- Fedoraware/TeamFortress2/TeamFortress2/Features/Chams/DMEChams.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Features/Chams/DMEChams.cpp
@@ -368,12 +368,12 @@ Chams_t GetPlayerChams(CBaseEntity* pEntity) {
 		if (g_EntityCache.IsFriend(pEntity->GetIndex()) && Vars::Chams::Players::Friend.chamsActive) {
 			return Vars::Chams::Players::Friend;
 		}
-		if (pEntity->GetTeamNum() != pLocal->GetTeamNum()) {
-		//if (pEntity->GetTeamNum() == 2) {
+		//if (pEntity->GetTeamNum() != pLocal->GetTeamNum()) {
+		if (pEntity->GetTeamNum() == 2) {
 			return Vars::Chams::Players::Enemy;
 		}
-		if (pEntity->GetTeamNum() == pLocal->GetTeamNum()) {
-		//if (pEntity->GetTeamNum() == 3) {
+		//if (pEntity->GetTeamNum() == pLocal->GetTeamNum()) {
+		if (pEntity->GetTeamNum() == 3) {
 			return Vars::Chams::Players::Team;
 		}
 	}
