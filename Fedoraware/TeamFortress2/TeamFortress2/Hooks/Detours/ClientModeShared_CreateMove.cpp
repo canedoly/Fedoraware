f6fe3dd53deb573bb2b12fee35f55cb07be72eac
canedoly <81353241+canedoly@users.noreply.github.com>
2022-11-12 13:28:48 +0100

    ahh

--- Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/ClientModeShared_CreateMove.cpp
+++ Fedoraware/TeamFortress2/TeamFortress2/Hooks/Detours/ClientModeShared_CreateMove.cpp
@@ -73,7 +73,7 @@ MAKE_HOOK(ClientModeShared_CreateMove, Utils::GetVFuncPtr(I::ClientModeShared, 2
 				G::Frozen = MaxSpeed == 1;
 			}
 
-			pCmd->viewangles = Vars::Test::ViewZ.Value;
+			// pCmd->viewangles = Vars::Test::ViewZ.Value;
 			// have fun being marked by every built in cheater detection which checks for z angles lol
 
 			// Update Global Info
